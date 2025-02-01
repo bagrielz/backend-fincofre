5086d3708f415aceb472a753f21009bcf9e16d26
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-02-01 17:20:09 -0300

    feat: add constructor in SpentListingDTO

--- src/main/java/br/com/fincofre/api/spent/SpentListingDTO.java
+++ src/main/java/br/com/fincofre/api/spent/SpentListingDTO.java
@@ -1,4 +1,9 @@
 package br.com.fincofre.api.spent;
 
 public record SpentListingDTO(String date, String spent, Status status, String value, Category category, String account, Method method) {
+
+    public SpentListingDTO(Spent spent) {
+        this(spent.getDate(), spent.getSpent(), spent.getStatus(), spent.getValue(), spent.getCategory(), spent.getAccount(), spent.getMethod());
+    }
+
 }

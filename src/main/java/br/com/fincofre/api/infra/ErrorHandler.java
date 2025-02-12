855a102c580ce850e01a9a473be3e4484cf72d74
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-02-12 13:34:49 -0300

    feat: add errorHandling400 method

--- src/main/java/br/com/fincofre/api/infra/ErrorHandler.java
+++ src/main/java/br/com/fincofre/api/infra/ErrorHandler.java
@@ -2,6 +2,8 @@ package br.com.fincofre.api.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -13,6 +15,21 @@ public class ErrorHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity errorHandling404() {
         return ResponseEntity.notFound().build();
+    }
+
+    // Método para tratar o erro 400
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity errorHandling400(MethodArgumentNotValidException ex) {
+        var err = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(err.stream().map(ErrorValidationDTO::new).toList());
+    }
+
+    // DTO para retornar os campos da resposta
+    private record ErrorValidationDTO(String field, String message) {
+        public ErrorValidationDTO(FieldError err) {
+            this(err.getField(), err.getDefaultMessage());
+        }
     }
 
 }

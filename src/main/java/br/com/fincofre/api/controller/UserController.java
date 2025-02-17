4edf2381fea6fa4b877daef09b8b2f4ce3bf32d8
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-02-17 13:35:02 -0300

    feat: add UserController

---
+++ src/main/java/br/com/fincofre/api/controller/UserController.java
@@ -0,0 +1,9 @@
+package br.com.fincofre.api.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/entrar")
+public class UserController {
+}

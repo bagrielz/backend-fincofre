15a6464c1715fb3c68b577413b864f2d96ee5159
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-02-20 14:45:46 -0300

    feat: add passwordEncoder method in SecurityConfigurations

--- src/main/java/br/com/fincofre/api/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/fincofre/api/infra/security/SecurityConfigurations.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,6 +27,12 @@ public class SecurityConfigurations {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    // Esse método configura o Spring a usar esse algoritmo como hash de senha
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }

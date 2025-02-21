5abd309a16c9ef9be5f4643131ec62c72cfcbff5
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-02-21 11:36:53 -0300

    feat: add the logic to authenticate the user
    This update includes:
    - Create the logic to generate the token JWT
    - Add TokenDTO

--- src/main/java/br/com/fincofre/api/controller/AuthController.java
+++ src/main/java/br/com/fincofre/api/controller/AuthController.java
@@ -1,6 +1,9 @@
 package br.com.fincofre.api.controller;
 
 import br.com.fincofre.api.domain.user.AuthDTO;
+import br.com.fincofre.api.domain.user.User;
+import br.com.fincofre.api.infra.security.TokenDTO;
+import br.com.fincofre.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,13 +21,17 @@ public class AuthController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid AuthDTO data) {
         // O método authenticate() do AuthenticationManager espera um objeto do tipo Authentication e UsernamePasswordAuthenticationToken é uma implementação concreta desse contrato
-        var token = new UsernamePasswordAuthenticationToken(data.login(), data.password()); // Cria o DTO UsernamePasswordAuthenticationToken e passa as informações de acesso
-        var auth = manager.authenticate(token); // Dispara o processo de autenticação
+        var authToken = new UsernamePasswordAuthenticationToken(data.login(), data.password()); // Cria o DTO UsernamePasswordAuthenticationToken e passa as informações de acesso
+        var auth = manager.authenticate(authToken); // Dispara o processo de autenticação
+        var token = tokenService.generateToken((User) auth.getPrincipal()); // Gera o token
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok(new TokenDTO(token));
     }
 
 }
--- src/main/java/br/com/fincofre/api/infra/security/TokenService.java
+++ src/main/java/br/com/fincofre/api/infra/security/TokenService.java
@@ -1,7 +1,37 @@
 package br.com.fincofre.api.infra.security;
 
+import br.com.fincofre.api.domain.user.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        var algorithm = Algorithm.HMAC256(secret);
+        try {
+            return JWT.create()
+                    .withIssuer("API FinCofre") // Nome da aplicação que gera o token
+                    .withSubject(user.getLogin()) // Pega o usuário que é proprietário do token
+                    .withExpiresAt(expirationDate()) // Validade do token
+                    .sign(algorithm);
+        } catch (JWTCreationException ex) {
+            throw new RuntimeException("Erro ao gerar token JWT", ex);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(24).toInstant(ZoneOffset.of("-03:00"));
+    }
+
 }

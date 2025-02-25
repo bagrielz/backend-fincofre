43784cf17af69639bbee861e0775bf6fd741b026
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-02-25 13:11:28 -0300

    ref: update the SecurityFilterChain
    This update includes:
    - Updates the code to release specific requests to the API
    - Updates the logic of the recoverToken method
    - Updates the filter method for Spring to save the token

--- src/main/java/br/com/fincofre/api/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/fincofre/api/infra/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.fincofre.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,16 +11,25 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity // Anotação que permite personalizar as configurações de segurança
 public class SecurityConfigurations {
+
+    @Autowired
+    private SecurityFilter securityFilter;
 
     // Esse método altera o modelo de autenticação para STATELESS
     @Bean // Anotação que expõe automaticamente o retorno do método para o Spring
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf(csrf -> csrf.disable())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(req -> {
+                    req.requestMatchers("/login").permitAll(); // Libera essa requisição para qualquer usuário acessar
+                    req.anyRequest().authenticated(); // Solicita autenticação para as outras requisições
+                })
+                .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
--- src/main/java/br/com/fincofre/api/infra/security/SecurityFilter.java
+++ src/main/java/br/com/fincofre/api/infra/security/SecurityFilter.java
@@ -1,10 +1,13 @@
 package br.com.fincofre.api.infra.security;
 
+import br.com.fincofre.api.domain.user.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,11 +19,21 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UserRepository repository;
+
     // Esse método filtra as chamadas à API
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = recoverToken(request);
-        var subject = tokenService.getSubject(token);
+
+        if (token != null) {
+            var subject = tokenService.getSubject(token);
+            var user = repository.findByLogin(subject);
+            var auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
 
         filterChain.doFilter(request, response);
     }
@@ -29,8 +42,8 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String recoverToken(HttpServletRequest request) {
         var authHeader = request.getHeader("Authorization");
 
-        if (authHeader == null) throw new RuntimeException("Token JWT não enviado no cabeçalho!");
+        if (authHeader != null) return authHeader.replace("Bearer ", "");
 
-        return authHeader.replace("Bearer ", "");
+        return null;
     }
 }

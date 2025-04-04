d848a7caf18a21463d19d10163555f24cd442dbe
Gabriel Stênio <gabriel.stenio@outlook.com>
2025-04-04 14:33:35 -0300

    ref: update the logic for calling requests from the SpentController class

--- src/main/java/br/com/fincofre/api/domain/spent/SpentUpdateDTO.java
+++ src/main/java/br/com/fincofre/api/domain/spent/SpentUpdateDTO.java
@@ -2,5 +2,5 @@ package br.com.fincofre.api.domain.spent;
 
 import jakarta.validation.constraints.NotNull;
 
-public record SpentUpdateDTO(@NotNull Long id, @NotNull Long userId, String date, String spent, Status status, String value, Category category, String account, Method method) {
+public record SpentUpdateDTO(@NotNull Long id, String date, String spent, Status status, String value, Category category, String account, Method method) {
 }

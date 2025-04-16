e7e9576372b0cd8fc095f218452437dc4cf0e283
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-04-16 09:44:52 -0300

    style: changes the name of the list method to detail

--- src/main/java/br/com/fincofre/api/models/dtos/UserDetailsDTO.java
+++ src/main/java/br/com/fincofre/api/models/dtos/UserDetailsDTO.java
@@ -4,7 +4,7 @@ import br.com.fincofre.api.models.entities.user.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UserDetailsDTO(@NotNull Long id, String login, String name, String email, String password) {
+public record UserDetailsDTO(Long id, String login, String name, String email, String password) {
 
     public UserDetailsDTO(User user) {
         this(user.getId(), user.getLogin(), user.getName(), user.getEmail(), user.getPassword());

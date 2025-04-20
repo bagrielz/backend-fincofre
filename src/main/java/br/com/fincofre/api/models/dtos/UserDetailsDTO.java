bb25126e7591da580692fc48a43d4481e25b4920
Gabriel Stênio <gabri.stnio50@hotmail.com>
2025-04-20 14:23:12 -0300

    ref: removes the password property from dtosanswers

--- src/main/java/br/com/fincofre/api/models/dtos/UserDetailsDTO.java
+++ src/main/java/br/com/fincofre/api/models/dtos/UserDetailsDTO.java
@@ -4,10 +4,10 @@ import br.com.fincofre.api.models.entities.user.User;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record UserDetailsDTO(Long id, String login, String name, String email, String password) {
+public record UserDetailsDTO(Long id, String login, String name, String email) {
 
     public UserDetailsDTO(User user) {
-        this(user.getId(), user.getLogin(), user.getName(), user.getEmail(), user.getPassword());
+        this(user.getId(), user.getLogin(), user.getName(), user.getEmail());
     }
 
 }
